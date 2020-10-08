8672f9466070072f6d928ca57ef98064fa5f9b88
David Mitchell <davidm@orthorehab.com>
2020-10-08 11:00:42 -0600

    javascript to add scrolling effect

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,10 @@
 new WOW().init();
+
+$(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if(scroll < 300){
+        $('.fixed-top').css('background', 'transparent');
+    } else{
+        $('.fixed-top').css('background', 'rgba(23, 162, 184, 0.9)');
+    }
+});
